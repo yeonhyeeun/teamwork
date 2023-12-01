16960e01bf6e655249556ad8128ce2fc4c725afa
Junghyun An <daniel7806@naver.com>
2023-12-01 18:52:06 +0900

    add progress bar in quiz page

--- lib/study_func/quiz.dart
+++ lib/study_func/quiz.dart
@@ -26,23 +26,20 @@ class _QuizPageState extends State<QuizPage> {
   }
 
   Future<void> fetchDataFromFirestore() async {
-    //quizData가 비어있을때만
-    if (quizData.isEmpty) {
-      try {
-        var snapshot = await FirebaseFirestore.instance
-            .collection('lecture')
-            .doc(widget.lectureId)
-            .get();
+    try {
+      var snapshot = await FirebaseFirestore.instance
+          .collection('lecture')
+          .doc(widget.lectureId)
+          .get();
 
-        List<Map<dynamic, dynamic>> fetchedQuizData =
-        List<Map<dynamic, dynamic>>.from(snapshot['questions']);
+      List<Map<dynamic, dynamic>> fetchedQuizData =
+      List<Map<dynamic, dynamic>>.from(snapshot['questions']);
 
-        setState(() {
-          quizData = fetchedQuizData;
-        });
-      } catch (e) {
-        print('Error fetching data from Firestore: $e');
-      }
+      setState(() {
+        quizData = fetchedQuizData;
+      });
+    } catch (e) {
+      print('Error fetching data from Firestore: $e');
     }
   }
 
@@ -155,19 +152,13 @@ class _QuizPageState extends State<QuizPage> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             LinearPercentIndicator(
-              alignment: MainAxisAlignment.center,
-              animation: true,
-              barRadius: Radius.circular(20),
               padding: EdgeInsets.zero,
-              percent: quizData.isNotEmpty
-                  ? (currentQuestionIndex + 1) / quizData.length.toDouble()
-                  : 0.0, // 수정된 부분
+              percent: (currentQuestionIndex + 1) / quizData.length.toDouble(),
               lineHeight: 10,
               backgroundColor: Colors.white,
               progressColor: CustomColor.brightRed,
-              width: 340,
+              width: MediaQuery.of(context).size.width,
             ),
-            SizedBox(height: 10,),
             Container(
               width: 400,
               height: 550,
